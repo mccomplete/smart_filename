82fd5460516161ade3aed0b128eb21430094b74c
Daniel Libicki <mccomplete@users.noreply.github.com>
2017-07-04 12:53:15 +0300

    Split read() and relpath().

--- smart_filename/__init__.py
+++ smart_filename/__init__.py
@@ -1,5 +1,5 @@
-def get_filename(smart_filename):
-    validate_smart_filename(smart_filename)
+def _preprocess(smart_filename):
+    _validate_smart_filename(smart_filename)
     return os.path.join(
         os.path.dirname(__file__),
         *[
@@ -8,7 +8,7 @@ def get_filename(smart_filename):
         ]
     )
 
-def validate_smart_filename(smart_filename):
+def _validate_smart_filename(smart_filename):
     directory = os.path.dirname(__file__)
     for path_element in smart_filename.split(os.sep):
         if path_element.startswith(".."):
@@ -18,13 +18,15 @@ def validate_smart_filename(smart_filename):
         else:
             directory = os.path.join(directory, path_element)
 
-def read(smart_filename):
+def _relpath(smart_filename):
     file_path_relative_to_project_root = get_filename(smart_filename)
     smart_filename_directory = path.dirname(__file__)
-    project_root = path.join(analytricks_common, "..")
-    file_path_relative_to_working_directory = path.join(
-        project_root,
-        file_path_relative_to_project_root,
-    )
-    with open(file_path_relative_to_working_directory) as f:
+    project_root = path.join(smart_filename_directory, "..")
+    return path.join(project_root, file_path_relative_to_project_root)
+    
+def _read(filename):
+    with open(filename) as f:
         return f.read()
+    
+def read(filename):
+    return _read(_preprocess(_relpath(filename)))
